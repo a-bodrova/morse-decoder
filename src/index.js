11f8608a6d973ea0cbb7ec1937435f08b1eb7d59
a-bodrova <soulwent94@gmail.com>
2021-08-23 18:24:32 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    let word = expr.slice(i, i + 10);
+    if (word === '**********') {
+      result += ' ';
+    } else {
+      let morse = '';
+      for (let j = 0; j < word.length; j += 2) {
+        let twins = word.slice(j, j + 2);
+        if (twins === '10') {
+           morse += '.';
+        } else if (twins === '11') {
+          morse += '-';
+        }
+      }      
+      result += MORSE_TABLE[morse];
+    }
+  }
+  return result;
 }
 
 module.exports = {
